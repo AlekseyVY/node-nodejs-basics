b0fa38ea6462bdd6ba055e2d11984159fc7e1288
Aleksey <AlekseyVY@gmail.com>
2022-06-05 19:08:58 +0300

    feat: streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,19 @@
+import fs from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import readline from "readline";
+
 export const write = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const dir = path.join(__dirname, '/files/fileToWrite.txt')
+    const rl = readline.createInterface({ input: process.stdin });
+    const writeStream = fs.createWriteStream(dir, {flags: 'a'});
+    rl.on('line', (input) => {
+        console.log(input)
+        writeStream.write(input);
+        writeStream.write('\n');
+    })
+
 };
+
+await write();
