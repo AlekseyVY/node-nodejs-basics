195783b2de189c6a0c7cca33799e57e3d3d90aac
Aleksey <AlekseyVY@gmail.com>
2022-06-05 18:26:08 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,23 @@
+import fs from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import * as hash from 'crypto';
+
+function catapultError(msg) {
+    throw new Error(msg);
+}
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const dir = path.join(__dirname, '/files/fileToCalculateHashFor.txt');
+    const errorMessage = 'FS operation failed';
+    const encoding = 'utf8';
+    const dirExistsCheck = await fs.promises.access(dir).then(() => true).catch(() => false);
+    dirExistsCheck ? await ( async () => {
+        const content = await fs.promises.readFile(dir, encoding)
+        const result = hash.createHash('sha256').update(content).digest('hex')
+        console.log(result);
+    })() : catapultError(errorMessage);
 };
+
+await calculateHash();
