26441a3d2f9181e5a5cc961c7e6486375b5e82b6
Aleksey <AlekseyVY@gmail.com>
2022-06-05 20:48:39 +0300

    fix: __dirname bug

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,11 +1,15 @@
 import { Worker } from 'worker_threads';
 import os from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 export const performCalculations = async () => {
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const workerPath = path.join(__dirname, 'worker.js');
     const cpuData = os.cpus().length;
     const runWorkerThread = (data) => {
         return new Promise((resolve, reject) => {
-            const worker = new Worker('./worker.js');
+            const worker = new Worker(workerPath);
             worker.postMessage(data);
             worker.on('message', resolve);
             worker.on('error', reject);
