f3d96845b658b4571a6fefe6c127d4a2e96c2294
Aleksey <AlekseyVY@gmail.com>
2022-06-05 21:33:45 +0300

    feat: child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,18 @@
+import { fork } from 'node:child_process';
+import path from "path";
+import {fileURLToPath} from "url";
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const dir = path.join(__dirname, '/files/script.js')
+    const argsToPass = args.slice(2);
+    const controller = new AbortController();
+    const { signal } = controller;
+    const child = fork(dir, argsToPass, { signal });
+    child.send(args)
+    child.on('message', (data) => {
+        console.log(data)
+    })
 };
+
+await spawnChildProcess(process.argv);
