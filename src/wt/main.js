8706264d5ad7692306cd7ff4458c92054be1c45d
Aleksey <AlekseyVY@gmail.com>
2022-06-05 20:43:24 +0300

    feat: worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,22 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+
 export const performCalculations = async () => {
-    // Write your code here
+    const cpuData = os.cpus().length;
+    const runWorkerThread = (data) => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker('./worker.js');
+            worker.postMessage(data);
+            worker.on('message', resolve);
+            worker.on('error', reject);
+        })
+    }
+    const resultArray = [];
+    for(let i = 0; i < cpuData; i++) {
+        resultArray.push(runWorkerThread(10 + i))
+    }
+    const result = await Promise.all(resultArray);
+    console.log(result);
 };
+
+await performCalculations();
