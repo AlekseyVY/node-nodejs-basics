be1f109a21b4496dd9879f05dc0b58c449c02a9d
Aleksey <AlekseyVY@gmail.com>
2022-06-05 15:38:04 +0300

    feat: copy and create fs

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,19 @@
+import fs from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+function catapultError(msg) {
+    throw new Error(msg);
+}
+
 export const create = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const dir = path.join(__dirname, '/files/fresh.txt')
+    const message = 'I am fresh and young';
+    const errorMessage = 'FS operation failed';
+    fs.access(dir, (error) => error
+        ? fs.writeFile(dir, message, () => true)
+        : catapultError(errorMessage))
 };
+
+await create();
